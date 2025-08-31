ec36e9d4f59d22b2882a1e9730072662a856ea87
VincentCurry <vincent.curry@receiptonmyphone.com>
2025-09-01 00:55:39 +0100

    Removed usings

--- CodeGenerator/StringExtensions.cs
+++ CodeGenerator/StringExtensions.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Messaging;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace CodeGenerator
+﻿namespace CodeGenerator
 {
     public static class StringExtensions
     {
