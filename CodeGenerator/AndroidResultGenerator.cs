59364b35306a6db0c47c761c53786d04f4386050
VincentCurry <vincent.curry@receiptonmyphone.com>
2025-08-31 23:07:58 +0100

    Added Result generator which has been smoke tested

---
+++ CodeGenerator/AndroidResultGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeGenerator
+{
+    public class AndroidResultGenerator : Generator
+    {
+        public AndroidResultGenerator(List<SQLTable> tables, string destinationFolder, string nameSpace) : base(tables, destinationFolder, nameSpace)
+        {
+            fileSuffix = "kt";
+            fileNameSuffix = "Result";
+        }
+
+        internal override void GenerateFilePerTable(SQLTable table)
+        {
+            classText.AppendLine($"package com.{_nameSpace}.ui.{table.Name.Decapitalise()}");
+            classText.Append(Environment.NewLine);
+
+            classText.AppendLine($"import com.{_nameSpace}.data.model.{table.Name}");
+            classText.Append(Environment.NewLine);
+
+            classText.AppendLine($"data class {table.Name}Result(");
+            classText.AppendLine($"\tval success: {table.Name}? = null,");
+            classText.AppendLine("\tval error: Int? = null");
+            classText.AppendLine($")");
+        }
+    }
+}
