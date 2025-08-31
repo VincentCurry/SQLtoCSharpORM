8166cc1c0ab9d759c101161784c5d7276f12d0b5
VincentCurry <vincent.curry@receiptonmyphone.com>
2025-08-31 12:01:50 +0100

    Added ApiRepositoryGenerator.

---
+++ CodeGenerator/AndroidApiRepositoryGenerator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeGenerator
+{
+    public class AndroidApiRepositoryGenerator : Generator
+    {
+        public AndroidApiRepositoryGenerator(List<SQLTable> tables, string destinationFolder, string nameSpace) : base(tables, destinationFolder, nameSpace)
+        {
+            fileSuffix = "kt";
+            fileNameSuffix = "Repository";
+        }
+
+        internal override void GenerateFilePerTable(SQLTable table)
+        {
+
+            classText.AppendLine($"package com.{_nameSpace}.data");
+            classText.Append(Environment.NewLine);
+
+            classText.AppendLine("import androidx.lifecycle.MutableLiveData");
+            classText.AppendLine($"import com.{_nameSpace}.data.model.{table.Name}");
+            classText.Append(Environment.NewLine);
+
+            classText.AppendLine($"class {table.Name}Repository (val dataSource: {table.Name}DataSource) {{");
+            classText.AppendLine($"\tvar {table.Name.Decapitalise()}Id: MutableLiveData<{table.PrimaryKey.kotlinDataType}> = MutableLiveData<{table.PrimaryKey.kotlinDataType}> ( \"\")");
+            classText.Append(Environment.NewLine);
+
+            classText.AppendLine($"\tsuspend fun fetch{table.Name}({table.Name.Decapitalise()}: {table.Name}) {{");
+            classText.AppendLine($"\t\tval result: Result<{table.Name}> = dataSource.get{table.Name}({table.Name.Decapitalise()})");
+            classText.Append(Environment.NewLine);
+
+            classText.AppendLine("\t\tif (result is Result.Success) {");
+            classText.AppendLine($"\t\t\t{table.Name.Decapitalise()}Id.value = result.data.{table.Name.Decapitalise()}Id");
+            classText.AppendLine("\t\t}");
+            classText.AppendLine("\t}");
+            classText.AppendLine("}");
+
+        }
+    }
+}
