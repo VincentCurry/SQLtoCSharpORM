cd8c8507551132becb18e2908b0a72d6b8bc9189
VincentCurry <vincent.curry@receiptonmyphone.com>
2025-09-04 10:39:40 +0100

    Fetch functions simply return the data, now, and various other fixes.

--- CodeGenerator/AndroidApiRepositoryGenerator.cs
+++ CodeGenerator/AndroidApiRepositoryGenerator.cs
@@ -18,7 +18,6 @@ namespace CodeGenerator
             classText.AppendLine($"package com.{_nameSpace}.data");
             classText.Append(Environment.NewLine);
 
-            classText.AppendLine("import androidx.lifecycle.MutableLiveData");
             classText.AppendLine($"import com.{_nameSpace}.data.model.{table.Name}");
             if (table.Columns.Any(col => col.cSharpDataType == "DateTime"))
             {
@@ -27,27 +26,15 @@ namespace CodeGenerator
             classText.Append(Environment.NewLine);
 
             classText.AppendLine($"class {table.Name}Repository (val dataSource: {table.Name}DataSource) {{");
-            classText.AppendLine($"\tvar {table.Name.Decapitalise()}s: MutableLiveData<List<{table.Name}>> = MutableLiveData<List<{table.Name}>> (null)");
-            classText.Append(Environment.NewLine);
 
-            classText.AppendLine($"\tsuspend fun fetch{table.Name}({table.Name.Decapitalise()}Id: {table.PrimaryKey.kotlinDataType}) {{");
-            classText.AppendLine($"\t\tval result: Result<{table.Name}> = dataSource.get{table.Name}({table.Name.Decapitalise()}Id)");
-            classText.Append(Environment.NewLine);
-
-            classText.AppendLine("\t\tif (result is Result.Success) {");
-            classText.AppendLine($"\t\t\t{table.Name.Decapitalise()}s.value = listOf(result.data)");
-            classText.AppendLine("\t\t}");
+            classText.AppendLine($"\tsuspend fun fetch{table.Name}({table.Name.Decapitalise()}Id: {table.PrimaryKey.kotlinDataType}): Result<{table.Name}> {{");
+            classText.AppendLine($"\t\treturn dataSource.get{table.Name}({table.Name.Decapitalise()}Id)");
             classText.AppendLine("\t}");
             classText.Append(Environment.NewLine);
 
-            classText.AppendLine($"\tsuspend fun fetchAll{table.Name}() {{");
-            classText.AppendLine($"\t\tval result: Result<List<{table.Name}>> = dataSource.getAll{table.Name}()");
-            classText.Append(Environment.NewLine);
-
-            classText.AppendLine("\t\tif (result is Result.Success) {");
-            classText.AppendLine($"\t\t\t{table.Name.Decapitalise()}s.value = result.data");
+            classText.AppendLine($"\tsuspend fun fetchAll{table.Name}s() : Result<List<{table.Name}>> {{");
+            classText.AppendLine($"\t\treturn dataSource.getAll{table.Name}()");
             classText.AppendLine("\t\t}");
-            classText.AppendLine("\t}");
             classText.Append(Environment.NewLine);
 
             classText.AppendLine($"\tsuspend fun save{table.Name}({Library.TableColumnsCode(table, Library.KotlinParameterNameAndType, false, true, true)}) : Result<{table.Name}> {{");
